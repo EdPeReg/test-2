78c01cd99ac47b137ee97f732014a611c3dc3ac1
EdPeReg <dardo1818@gmail.com>
2021-12-08 21:26:05 -0600

    modified?

--- process.h
+++ process.h
@@ -1,13 +1,3 @@
-#include <iostream>
-
 class Process {
-    public:
-        Process() {  }
-        void hi()
-        {
-            std::cout << "hola";
-        }
 
-        ~Process() { }
-    private:
 };
