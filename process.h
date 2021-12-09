09b1dc229fe3075de01fcdcca456f454ed07f283
EdPeReg <dardo1818@gmail.com>
2021-12-08 21:09:12 -0600

    just hi function

--- process.h
+++ process.h
@@ -1,6 +1,12 @@
+#include <iostream>
+
 class Process {
     public:
         Process() {  }
+        void hi()
+        {
+            std::cout << "hola";
+        }
 
         ~Process() { }
     private:
